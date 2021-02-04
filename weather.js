4f36fe1a54e46c9ce4308fbbff3f8fa800e4a4ab
sabreenshehwar <76182477+sabreenshehwar@users.noreply.github.com>
2021-02-04 10:51:43 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,16 +1,16 @@
 
-function search(event) {
-    event.preventDefault();
-    let cityElement = document.querySelector("h3");
-    let cityInput = document.querySelector("#city-input");
-    cityElement.innerHTML = cityInput.value;
-    searchCity(cityInput.value);
+unction search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("h3");
+  let cityInput = document.querySelector("#city-input");
+  cityElement.innerHTML = cityInput.value;
+  searchCity(cityInput.value);
 }
 function displayForecast(response) {
-    let forecastElement = document.querySelector("#forecast");
-    let forecast = response.data.list[0];
-    console.log(forecast);
-    forecastElement.innerHTML = `
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.list[0];
+  console.log(forecast);
+  forecastElement.innerHTML = `
        <div class="row weather-forecast"id="forecast">
                     <div class="col">
                       <div id="Weekday1">
@@ -28,89 +28,76 @@ function displayForecast(response) {
             </div>
             `;
 }
-
 function searchCity(city) {
-    let apiKey = "ae84fe76c05b33cdc51b77049718e2b3";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(`${apiUrl}`).then(showTemperature);
-    axios.get(`${apiUrl}`).then(showDescription);
-
-    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayForecast);
-
+  let apiKey = "ae84fe76c05b33cdc51b77049718e2b3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then(showTemperature);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 let citySearch = document.querySelector("#search-form");
 citySearch.addEventListener("submit", search);
-
 function realDate() {
-    let now = new Date();
-    let date = now.getDate();
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let year = now.getFullYear();
-    let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ];
-    let day = days[now.getDay()];
-    let months = [
-        "January",
-        "Febuary",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "Decemeber"
-    ];
-    let month = months[now.getMonth()];
-    return `${day}, ${month} ${date}, ${year} @ ${hours}:${minutes}`;
+  let now = new Date();
+  let date = now.getDate();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let year = now.getFullYear();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "Decemeber",
+  ];
+  let month = months[now.getMonth()];
+  return `${day}, ${month} ${date}, ${year} @ ${hours}:${minutes}`;
 }
 let h4 = document.querySelector("h4");
 h4.innerHTML = realDate();
-
 function showTemperature(response) {
-    console.log(response.data);
-    let temperature = Math.round(response.data.main.temp);
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = `${temperature}°C`;
-    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-    city.innerHTML = `${newCity}`;
-    temperatureElement.innerHTML = `${temperature}`;
-}
-
-function showDescription(response) {
-    console.log(response.data);
-    let description = response.data.weather.description;
-    document.querySelector("#weather-description").innerHTML = response.data.weather[0].description;
-    weatherdescription.innerHTML = `(${description})`;
+  console.log(response.data);
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${temperature}°C`;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  let cityElement = document.querySelector("h3");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = `${temperature}`;
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
 }
 function findPosition(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let apiKey = "ae84fe76c05b33cdc51b77049718e2b3";
-    let unit = "metric";
-    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
-    let apiUrl = `${apiEndpoint}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
-
-    axios.get(apiUrl).then(showTemperature);
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "ae84fe76c05b33cdc51b77049718e2b3";
+  let unit = "metric";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiUrl = `${apiEndpoint}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(showTemperature);
 }
-
 function getCurrentPosition() {
-    navigator.geolocation.getCurrentPosition(findPosition);
+  navigator.geolocation.getCurrentPosition(findPosition);
 }
-
 let button = document.querySelector("button");
 button.addEventListener("click", getCurrentPosition);
-
-
