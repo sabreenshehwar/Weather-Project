c9e601a30a29b33acbcc5e841a9deadd196175e0
sabreenshehwar <76182477+sabreenshehwar@users.noreply.github.com>
2021-02-11 19:40:39 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -14,16 +14,8 @@ function searchCity(city) {
 }
 let citySearch = document.querySelector("#search-form");
 citySearch.addEventListener("submit", search);
-
-function showtoday(event) {
-    event.preventDefault();
-    let todayElement = document.querySelector("h4");
-    todayElement.innerHTML = formatDate.value;
-    show(formatDate.value);
-}
-
 function formatDate(timestamp) {
-  let date = new date(timestamp);
+  let date = new Date(timestamp);
   let days = [
     "Sunday",
     "Monday",
@@ -50,8 +42,13 @@ function formatDate(timestamp) {
   let year = now.getFullYear();
   let month = months[now.getMonth()];
   let day = days[date.getDay()];
-  return `${day} ${month} ${date} ${year} @ ${formatHours(timestamp)}`;
+  return `${day} ${month} ${date.getDate()} ${year} @ ${formatHours(
+    timestamp
+  )}`;
 }
+let now = new Date();
+let todayElement = document.querySelector("h4");
+todayElement.innerHTML = formatDate(now);
 function formatHours(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -114,33 +111,26 @@ function findPosition(position) {
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
   let apiUrl = `${apiEndpoint}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
   axios.get(apiUrl).then(showTemperature);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(findPosition);
 }
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityinputElement = document.querySelector("#city-input");
-  search(cityinputElement.value);
-}
 function showFahrenheitTemperature(event) {
   event.preventDefault();
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  alert(fahrenheitTemperature);
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  temperatureElement.innerHTML = `${Math.round(fahrenheitTemperature)}°F`;
 }
 function showCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  temperatureElement.innerHTML = `${Math.round(celsiusTemperature)}°C`;
 }
 let celsiusTemperature = null;
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 searchCity("new york");
-
